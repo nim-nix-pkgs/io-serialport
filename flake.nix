5c6ef1c00284aabfd8cede009f010e72d2441697
hugosenari <hugosenari@gmail.com>
2022-03-12 02:00:54 -0300

    chore: re index io-serialport

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Obsolete - please use serialport instead!'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."io-serialport-master".url = "path:./master";
+  inputs."io-serialport-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."io-serialport-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
